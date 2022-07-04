18aa87ee6a9d62b9bfbe2fd2b9e2af457769f0d3
NikUsov1998 <nik_usov.1998@mail.ru>
2022-07-05 01:42:29 +0500

    Добавлена пузырьковая сортировка Добавлена заготовка функции пузырьковой сортировки по указателям Добавлена обрезка неиспользуемой ёмкости вектора при его заполнении

--- Links/Main.cpp
+++ Links/Main.cpp
@@ -9,14 +9,17 @@ void FillArray(int numArr[], const int SIZE)
 		numArr[i] = rand(); // Генератор рандомных чисел
 	}
 }
-
-void FillArray(std::vector<int>&arr, int i)
+void FillArray(std::vector<int>&arr, int cap)
 {
-	for (auto i = 0; i < arr.size(); i++)
+	for (auto i = 0; i < cap; ++i)
 	{
+		//if (i >= arr.capacity())
+		//	arr.resize(i + 1);
 		arr.push_back(rand());  // Генератор рандомных чисел
 	}
+	arr.shrink_to_fit();
 }
+
 void LinkPrint(int numArr[], const int SIZE)
 {
 	std::cout << "Adress: \tData: " << "\n";
@@ -34,42 +37,42 @@ void LinkPrint(std::vector<int> arr)
 	}
 }
 
-void SimpleSort(std::vector<int>& arr)
+void BubbleSort(std::vector<int>& arr)
 {
-	for (auto i = 0; i < arr.size(); ++i)
-	{
-		for (auto j = i; j < arr.size(); ++j)
-		{
-			if (arr[i] < arr[j])
+	int i, j;
+	for (i = 0; i < arr.size() - 1; i++)
+		for (j = 0; j < arr.size() - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+				std::swap(arr[j], arr[j + 1]);
+}
+void LinksBubbleSort(std::vector<int>&arr)
+{
+	int i, j;
+	int* pi = &i;
+	int* pj = &j;
+	for (i = 0; i < arr.size() - 1; i++)
+		for (j = 0; j < arr.size() - i - 1; j++)
+			if (arr[*pi] > arr[*pj + 1])
 			{
-				auto tmp = arr[i];
+				int tmp = arr[j];
 				arr[j] = arr[i];
-				arr[j] = tmp;
+				arr[i] = tmp;
 			}
-		}
-	}
-}
-
-void LinksSort(int numArr[], const int SIZE)
-{
-
-}
-void Swap(std::vector<int>& arr)
-{
-	arr.swap(arr);
-}
-void Shrink(std::vector<int>& arr)
-{
-	arr.shrink_to_fit();
 }
 
 int main()
 {
 	srand(time(NULL)); // Отсчёт секунд от 1970 года для генерации по настоящему случайных чисел
-	std::vector<int> numArr;
-	FillArray(numArr, 100000);
-	Swap(numArr); // меняет ёмкость вектора
-	SimpleSort(numArr);
-	LinkPrint(numArr);
+	std::vector<int> numArr1, numArr2;
+	FillArray(numArr1, 10);
+	FillArray(numArr2, 10);
+	BubbleSort(numArr1);
+	LinksBubbleSort(numArr2);
+
+	//std::vector<int>* pointerArr = &numArr;
+	//int* pnA = &numArr[0];
+	//LinksBubbleSort(pointerArr);
+	//LinkPrint(numArr1);
+	//std::cout << numArr1[0] << '\t' << &numArr1[0] << '\t' << std::endl;
 	return 0;
 }
